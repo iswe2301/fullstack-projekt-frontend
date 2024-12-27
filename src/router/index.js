d9c6658cfe3d3fd0d3f2e71d2fe82f3590d71260
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-27 15:25:44 +0100

    Lagt till autentisering och skyddade rutter i router-konfigurationen:
    - Skapat routes för "Login", "Produkter" och "Lägg till produkt".
    - Implementerat `beforeEach`-skydd för att kontrollera om användaren är inloggad innan åtkomst till skyddade sidor.
    - Integrerat autentiseringsstore för att verifiera användarens status.
    - Vid misslyckad autentisering skickas användaren tillbaka till inloggningssidan.

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import { useAuthStore } from '../stores/auth' // Importera store för autentisering
 
+// Skapa en router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login', // Starsidan (logga in)
+      component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/ProductView.vue'),
+      meta: { requiresAuth: true } // Skyddad sida
+    },
+    {
+      path: '/add-product',
+      name: 'product',
+      component: () => import('../views/AddProductView.vue'),
+      meta: { requiresAuth: true } // Skyddad sida
     },
   ],
 })
+
+// Kontrollera om användaren är inloggad innan de får tillgång till skyddade sidor
+router.beforeEach(async (to, from, next) => {
+
+  const authStore = useAuthStore() // Hämta store för autentisering
+
+  // Kontrollera om sidan kräver autentisering
+  if (to.meta.requiresAuth) {
+    await authStore.checkAuth(); // Kontrollera om användaren är inloggad (anropa metod)
+
+    // Kontrollera om användaren är inloggad
+    if (authStore.isLoggedIn) {
+      next(); // Tillåt användare att se sidan om de är inloggade
+    } else {
+      next('/'); // Skicka användaren till inloggning
+    }
+
+  } else {
+    next(); // För icke-skyddade sidor
+  }
+});
 
 export default router
