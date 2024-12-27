fb5deb73bf928cca8c6a2e6317f985523e145568
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-27 15:37:47 +0100

    Skapat vy för produktsidan

---
+++ src/views/ProductView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Produkter</h1>
+    <p>Skyddad produktsida...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductView",
+};
+</script>
+
+<style></style>
