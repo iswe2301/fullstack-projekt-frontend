290d53b8411020577b3d25bf21c741c7e45cfa6e
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-27 15:37:25 +0100

    Skapat vy för att lägga till produkter

---
+++ src/views/AddProductView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Lägg till produkt</h1>
+        <p>Skyddad sida...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddProductView",
+};
+</script>
+
+<style></style>
